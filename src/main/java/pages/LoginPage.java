8f1cdef5b0de77b1d04280d23e38d04579bbbfd8
VinitKulkarni <42830279+VinitKulkarni@users.noreply.github.com>
2026-02-24 10:48:41 +0530

    Update LoginPage.java

--- src/main/java/pages/LoginPage.java
+++ src/main/java/pages/LoginPage.java
@@ -1,1 +1,48 @@
+package pages;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class LoginPage {
+	//Web elements
+	@FindBy (xpath = "//input[@id='user-name']") private WebElement userName;
+	@FindBy (xpath = "//input[@id='password']") private WebElement password;
+	@FindBy (xpath = "//input[@id='login-button']") private WebElement loginButton;
+	@FindBy (xpath = "//h3[text()='Epic sadface: Username and password do not match any user in this service']") private WebElement invalidErrorMsg;
+	@FindBy (xpath = "//h3[text()='Epic sadface: Username is required']") private WebElement emptyErrorMsg;
+	
+	
+	//constructor
+	public LoginPage(WebDriver driver) {
+		PageFactory.initElements(driver, this);
+	}
+		
+		
+	//methods
+	public void setUserName(String uName) {
+		userName.sendKeys(uName);
+	}
+		
+	public void setPassword(String passw) {
+		password.sendKeys(passw);
+	}
+		
+	public void clickOnLoginButton(WebDriver driver) {
+		loginButton.click();
+	}
+
+	public String getInvalidErroMsg() {
+		return invalidErrorMsg.getText();
+	}
+	
+	public void clearBothFields() {
+		userName.clear();
+		password.clear();
+	}
+	
+	public String getEmptyErrorMsg() {
+		return emptyErrorMsg.getText();
+	}
+}
